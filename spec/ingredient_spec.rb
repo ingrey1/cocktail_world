5b1d321df576c62e96e909d0c6fcc9683c53752b
github username <github email address>
2019-12-30 17:28:46 -0800

    Added a load_rake file to make use of rake tasks in the spec

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,7 +1,14 @@
 require "pry"
+require "rake"
 require_relative "../config/environment.rb"
+require_relative "./load_rake.rb"
 
 describe Ingredient do
+  before(:each) do
+    Rake::Task["db:drop"].invoke
+    Rake::Task["db:migrate"].invoke
+  end
+
   it("should initialize without throwing an error") do
     expect { Ingredient.new }.to_not raise_error
   end
@@ -12,10 +19,15 @@ describe Ingredient do
     end
   end
 
-  context "After a new instance has been created, instance attributes are accessible" do
+  context "After a new instance has been created, instance attributes have setters/getters" do
     ingredient1 = Ingredient.new(name: "Sugar")
     it("should have an accessible name instance attribute") do
       expect(ingredient1.name).to eq("Sugar")
+    end
+
+    it("should have a settable name instance attribute") do
+      ingredient1.name = "Candy"
+      expect(ingredient1.name).to eq("Candy")
     end
     context "After a new instance is created, active-record methods are available to the instance" do
       ingredient2 = Ingredient.new(name: "Spice")
---
+++ spec/load_rake.rb
@@ -0,0 +1,4 @@
+rake = Rake::Application.new
+Rake.application = rake
+rake.init
+rake.load_rakefile
