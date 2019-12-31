5a8b5be182545bc00032944309e2e63e18174fd0
github username <github email address>
2019-12-31 14:40:46 -0800

    Added #cocktail_count to Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,7 +5,12 @@ class Ingredient < ActiveRecord::Base
     CocktailIngredient.all.select { |ci| ci.ingredient == self }.map { |ci| ci.cocktail }
   end
 
-  def cocktail_names
+  def cocktail_count
      cocktails.count
   end 
+
+  def cocktail_names
+    cocktails.map {|cocktail| cocktail.name}
+  end 
+
 end
