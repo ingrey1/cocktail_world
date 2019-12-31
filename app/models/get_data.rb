e77e74685d5723a81b09cd5e3daf49827d0ff455
Donny <edl1155@gmail.com>
2019-12-30 17:32:43 -0800

    work on getting all cocktails in get_data.rb

--- app/models/get_data.rb
+++ app/models/get_data.rb
@@ -6,8 +6,14 @@ class GetData
   
 
   def get_data
-    response = RestClient.get(https://www.thecocktaildb.com/api.php)
-    data_hash = JSON.parse(response)
+    # response = RestClient.get(https://www.thecocktaildb.com/api.php)
+    # data_hash = JSON.parse(response)
+
+    # get an array of all letters:
+    all_letters = ('a'..'z').to_a
+
+    # declare empty hash
+    drinks = {}
   end
 
 end
