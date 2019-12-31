e1644aea062e6018eb6236857bb53dc2a3378e2c
github username <github email address>
2019-12-30 17:17:00 -0800

    Added method signatures for PopulateDB class

--- app/models/populate_db.rb
+++ app/models/populate_db.rb
@@ -1,4 +1,38 @@
 class PopulateDB
 
+    attr_reader :cocktail_data, :cocktails, :ingredients, :cocktail_ingredients 
+
+    def initialize(cocktail_data)
+      @cocktail_data = cocktail_data
+      @ingredients = []
+      @cocktails = []
+      @cocktail_ingredients = []
+    end
+    
+    def build_cocktails
+       
+    end
+    
+    def build_ingredients 
+
+    end 
+
+    def build_cocktail_ingredients
+
+    end 
+
+    def populate_cocktails
+
+    end 
+
+    def populate_ingredients
+    
+    end
+    
+    def populate_db 
+      populate_cocktails()
+      populate_ingredients()
+      populate_cocktail_ingredients()
+    end 
 
 end 
