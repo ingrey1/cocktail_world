9b9d2bd270d00b200a993894b7ab133f37007a9b
github username <ingrey1@gmail.com>
2020-01-02 14:52:56 -0800

    Added methods to retrieve information about ingredients for a cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,12 +1,19 @@
 class Cocktail < ActiveRecord::Base
-  
   has_many :ingredients, through: :cocktail_ingredients
 
-
-  def ingredients 
+  def cocktail_ingredients
     CocktailIngredient.where(ingredient: self)
   end
-  
-  
-  
-end 
+
+  def ingredients
+    cocktail_ingredients.map { |ci| ci.ingredient }
+  end
+
+  def ingredients_count
+    ingredients.size
+  end
+
+  def ingredient_names
+    ingredients.map { |ingredient| ingredient.name }
+  end
+end
