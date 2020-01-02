f7f1615b081682a39da579de321405a9f3b6481e
github username <ingrey1@gmail.com>
2020-01-02 14:49:07 -0800

    added ingredients method to Cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,12 @@
 class Cocktail < ActiveRecord::Base
   
   has_many :ingredients, through: :cocktail_ingredients
+
+
+  def ingredients 
+    CocktailIngredient.where(ingredient: self)
+  end
+  
+  
   
 end 
