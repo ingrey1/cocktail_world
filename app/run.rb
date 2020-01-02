ea598ec3fd84c9844bb491a54ddd51f75e8a3d68
github username <ingrey1@gmail.com>
2020-01-02 11:53:04 -0800

    Set logger level 1 to remove all the sql output in console

--- app/run.rb
+++ app/run.rb
@@ -6,5 +6,5 @@ require_relative "../config/environment.rb"
 #popDB = PopulateDB.new(all_cocktails)
 #popDB.populate_db
 runner = RunCocktailWorld.new
-result = runner.retrieve_cocktails_by_ingredient("Vodka")
+result = runner.retrieve_cocktail_by_name("A1")
 puts result
